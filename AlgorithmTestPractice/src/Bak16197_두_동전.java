48e63dfffb94c573b567303514530a8bf9447c3b
Jisue <49144667+Jisue@users.noreply.github.com>
2022-05-14 19:08:29 +0900

    두 동전 bfs로 풀이

--- AlgorithmTestPractice/src/Bak16197_두_동전.java
+++ AlgorithmTestPractice/src/Bak16197_두_동전.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 /*
@@ -16,6 +17,26 @@ class Coin {
     Coin(int _x, int _y) {
         this.x = _x;
         this.y = _y;
+    }
+}
+
+class Move implements Comparable<Move>{
+    int x1;
+    int x2;
+    int y1;
+    int y2;
+    int count;
+    Move(int x1, int x2, int y1, int y2, int count) {
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this.y2 = y2;
+        this.count = count;
+    }
+
+    @Override
+    public int compareTo(Move arg0) {
+        return count - arg0.count;
     }
 }
 
@@ -43,49 +64,51 @@ public class Bak16197_두_동전 {
             }
         }
         coinCheck.add("" + coin.get(0).x + coin.get(1).x + coin.get(0).y + coin.get(1).y);
-        findMin(coin.get(0).x,coin.get(1).x, coin.get(0).y, coin.get(1).y, 1);
-        System.out.println(-1);
+        int result = findMin(coin.get(0).x,coin.get(1).x, coin.get(0).y, coin.get(1).y);
+        System.out.println(result);
     }
-    public static void findMin(int x1, int x2, int y1, int y2, int cnt) {
-//        System.out.println("코인체크");
-//        System.out.println(coinCheck);
+    public static int findMin(int x1, int x2, int y1, int y2) {
+        PriorityQueue<Move> pq = new PriorityQueue<>();
+        pq.offer(new Move(x1,x2,y1,y2,0));
         // 4방향으로 이동시킴
-        if(cnt > 10) {
-            return;
-        }
-        for(int i=0;i<4;i++) {
-            int nextX1 = x1 + dx[i];
-            int nextX2 = x2 + dx[i];
-            int nextY1 = y1 + dy[i];
-            int nextY2 = y2 + dy[i];
-            if(checkOut(nextX1, nextY1) && checkOut(nextX2, nextY2)) {
+        while(!pq.isEmpty()) {
+            Move m = pq.poll();
+            if(m.count >= 10) {
                 continue;
             }
-            if(checkOut(nextX1, nextY1) || checkOut(nextX2, nextY2)) {
-                System.out.println(cnt);
-                System.exit(0);
-                return;
-            }
-            // 동전 이동 못함!
-            if(board[nextX1][nextY1].equals("#") && board[nextX2][nextY2].equals("#")) {
-                continue;
-            }
-            // 첫번째 동전 이동
-            if (board[nextX1][nextY1].equals("#")) {
-                nextX1 = x1;
-                nextY1 = y1;
-            }
-            // 두번째 동전 이동
-            if(board[nextX2][nextY2].equals("#")) {
-                nextX2 = x2;
-                nextY2 = y2;
-            }
-            String newCoin = "" + nextX1 + nextX2 + nextY1 + nextY2;
-            if(!coinCheck.contains(newCoin)) {
-                coinCheck.add(newCoin);
-                findMin(nextX1, nextX2, nextY1, nextY2, cnt + 1);
+            for(int i=0;i<4;i++) {
+                int nextX1 = m.x1 + dx[i];
+                int nextX2 = m.x2 + dx[i];
+                int nextY1 = m.y1 + dy[i];
+                int nextY2 = m.y2 + dy[i];
+                if(checkOut(nextX1, nextY1) && checkOut(nextX2, nextY2)) {
+                    continue;
+                }
+                if(checkOut(nextX1, nextY1) || checkOut(nextX2, nextY2)) {
+                    return m.count + 1;
+                }
+                // 동전 이동 못함!
+                if(board[nextX1][nextY1].equals("#") && board[nextX2][nextY2].equals("#")) {
+                    continue;
+                }
+                // 첫번째 동전 이동
+                if (board[nextX1][nextY1].equals("#")) {
+                    nextX1 = m.x1;
+                    nextY1 = m.y1;
+                }
+                // 두번째 동전 이동
+                if(board[nextX2][nextY2].equals("#")) {
+                    nextX2 = m.x2;
+                    nextY2 = m.y2;
+                }
+                String newCoin = "" + nextX1 + nextX2 + nextY1 + nextY2;
+                if(!coinCheck.contains(newCoin)) {
+                    coinCheck.add(newCoin);
+                    pq.offer(new Move(nextX1, nextX2, nextY1, nextY2, m.count + 1));
+                }
             }
         }
+        return -1;
     }
     public static boolean checkOut(int x, int y) {
         if(x >= N || x < 0 || y >= M || y < 0) {
