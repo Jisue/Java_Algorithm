dba407561ccea12f1a0b7002c357896b5dae50e8
Jisue <49144667+Jisue@users.noreply.github.com>
2022-06-20 19:19:22 +0900

    백준 텀 프로젝트 풀이

--- AlgorithmTestPractice/src/Bak9466_텀_프로젝트.java
+++ AlgorithmTestPractice/src/Bak9466_텀_프로젝트.java
@@ -1,41 +1,57 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Bak9466_텀_프로젝트 {
     public static int total = 0;
+    public static boolean[] visit;
     public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         for(int t=0;t<T;t++) {
             int N = Integer.parseInt(br.readLine());
             StringTokenizer st = new StringTokenizer(br.readLine());
             int[] student = new int[N+1];
+            visit = new boolean[N+1];
             for(int i=1;i<=N;i++) {
                 student[i] = Integer.parseInt(st.nextToken());
-            }
-            findTeam(student, N);
-            System.out.println(total);
-        }
-    }
-    public static void findTeam(int[] student, int N) {
-        boolean[] visit = new boolean[N+1];
-        for(int i=1;i<=N;i++) {
-            int count = 0;
-            if(!visit[i]) {
-                int index = student[i];
-                while(true) {
-                    if(index == student[index]) {
-                        break;
-                    }
-                    index = student[index];
-                    if(index == student[i]) {
-                        total++;
-                        break;
-                    }
+                if(i == student[i]) {
+                    visit[i] = true;
+                    total++;
                 }
             }
+            findTeam(student, N);
+            sb.append(N-total + "\n");
+            total = 0;
+        }
+        System.out.println(sb.toString());
+    }
+    public static void findTeam(int[] student, int N) {
+        for(int i=1;i<=N;i++) {
+            if(visit[i]) {
+                continue;
+            }
+            int index = i;
+            ArrayList<Integer> arr = new ArrayList<>();
+            while(!visit[index]) {
+                visit[index] = true;
+                arr.add(index);
+                index = student[index];
+                if(visit[index] && arr.contains(index)) {
+                    total = total + arr.size() - arr.indexOf(index);
+                }
+            }
+//            System.out.println(arr);
         }
     }
 }
+/*
+1
+5
+3 3 1 2 1
+ */
